8189b31bcd22703c0a4d8dbd728a3137a1a566c1
Jacob San Juan <contactjsj42@gmail.com>
2021-04-08 16:51:48 -0400

    Final readthrough before submission

--- contracts/ContractProxy.sol
+++ contracts/ContractProxy.sol
@@ -20,8 +20,7 @@ contract ContractProxy is Proxy, Ownable {
 
   /**
    * @dev overridden from Parent contract to make sure that onlyOwner can
-   *  use ContractLogic.sol.  This will stop others from being able to transfer
-   *  tokens out.
+   *  use ContractLogic.sol and transfer funds
    */
   fallback() external payable virtual override onlyOwner {
     _fallback();
@@ -30,7 +29,6 @@ contract ContractProxy is Proxy, Ownable {
   /**
    * @dev overridden from Parent contract to make sure that onlyOwner can
    *  call and to revert because this is a wallet specifically for ERC20 tokens.
-   * This will stop others from being able to transfer tokens out.
    */
   receive() external payable virtual override onlyOwner {
     revert("Wallet is for ERC20 tokens only");
@@ -39,7 +37,7 @@ contract ContractProxy is Proxy, Ownable {
   /**
    * @return implementation address to caller
    */
-  function getImplementation() external view returns (address) {
+  function getImplementation() external view virtual returns (address) {
     return implementationAddr;
   }
 
