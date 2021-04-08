b7ed0f766ac8631324cfb7740718499c6be4a564
Jacob San Juan <contactjsj42@gmail.com>
2021-04-08 09:18:02 -0400

    Initial contract commit

---
+++ contracts/ContractLogic.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract ContractLogic {
+
+  function transfer(
+    address _tokenAddress,
+    address _to,
+    uint amount
+  )
+    external
+    virtual
+  {
+    IERC20 token = IERC20(_tokenAddress);
+    token.approve(address(this), amount); // Might have to be seperate call
+    token.transferFrom(msg.sender, _to, amount);
+  }
+}
---
+++ contracts/ContractProxy.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/proxy/Proxy.sol";
+
+contract ContractProxy is Proxy, Ownable {
+
+  address private implementationAddr;
+
+  constructor(address _implementationAddr) Ownable() {
+    implementationAddr = _implementationAddr;
+  }
+
+  function _implementation() internal view virtual override returns (address) {
+    return implementationAddr;
+  }
+}
